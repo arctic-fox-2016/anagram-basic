86aa303e45bb48d6e2d409748b1de81439533617
Ahyana Rizky Pratama & Ari Adiprana <pair+ahyanarizky.ariadiprana@hacktiv8.com>
2016-09-20 17:16:32 +0700

    release 1

--- helpers/util.js
+++ helpers/util.js
@@ -1,10 +1,21 @@
 'use strict';
-
+let fs = require("fs")
 var models = require('../models/index');
 var util = {};
+var AnagramChecker = require('anagram-checker');
+var content=fs.readFileSync("db/fixtures/words.js").toString().split('\n');
+
+var result=[]
 
 util.anagrams = function(source, callback){
+  result=[]
+  for(let i in content){
+    if(AnagramChecker(source,content[i]))
+      result.push(content[i])
+  }
 
+
+  callback(source, result);
 }
 
 module.exports = util;
