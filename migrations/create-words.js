36c8b2750bac91e9dd7ccc367c8055ec15f38e16
Ahyana Rizky Pratama & Ari Adiprana <pair+ahyanarizky.ariadiprana@hacktiv8.com>
2016-09-20 16:15:17 +0700

    release 1

---
+++ migrations/create-words.js
@@ -0,0 +1,30 @@
+'use strict';
+module.exports = {
+  up: function(queryInterface, Sequelize) {
+    return queryInterface.createTable('Words', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      word: {
+        type: Sequelize.STRING
+      },
+      additionalWord: {
+        type: Sequelize.STRING
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: function(queryInterface, Sequelize) {
+    return queryInterface.dropTable('Words');
+  }
+};
