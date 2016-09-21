88c38e330ae827b23ff8096665ab51ed5b344ca5
Peter Raswono <email": null>
2016-09-21 10:19:45 +0700

    Peter

--- routes/index.js
+++ routes/index.js
@@ -2,10 +2,23 @@ var express = require('express');
 var router = express.Router();
 var helper = require('../helpers/util');
 
+let fs = require("fs")
+
 /* GET home page. */
 
 router.get('/', function(req, res, next) {
-  res.render('index', {title: 'Anagrams'});
+  fs.readFile('./db/fixtures/words', (err, data) => {
+
+    let obj = []
+
+    for (let i = 0; i < data.length; i++) {
+      obj.push({
+        word: data[i]
+      })
+    }
+
+    res.render('index', {title: obj});
+  });
 });
 
 router.get('/:word', function(req, res, next) {
